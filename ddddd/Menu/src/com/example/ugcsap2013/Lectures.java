4824b3f474cbfe298a5e0e4c6f5bef50874b0507
Amit Bose <arunbaha@gmail.com>
2013-03-14 01:31:20 +0530

    dfdf
    fff

---
+++ ddddd/Menu/src/com/example/ugcsap2013/Lectures.java
@@ -0,0 +1,15 @@
+package com.example.ugcsap2013;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Lectures extends Activity{
+	
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lectures);
+		
+    }
+	
+	
+}
